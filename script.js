85df9656800059b6c436a8fe1fed4b11debd3c43
anubha <anubha.rajput@innotechteam.in>
2024-04-09 14:34:05 +0530

    update layout

--- script.js
+++ script.js
@@ -1,90 +1,129 @@
-let startBtn = document.getElementById('start-btn'); 
-let stopBtn = document.getElementById('stop-btn'); 
-let resetBtn = document.getElementById('reset-btn'); 
-  
-let hour = 0; 
-let minute = 0; 
-let second = 0; 
-let count = 0; 
-  
-startBtn.addEventListener('click', function () { 
-    timer = true; 
-    timeConvertor(Date.now());
-    stopWatch(); 
-}); 
-  
-stopBtn.addEventListener('click', function () { 
-    timer = false; 
-}); 
-  
-resetBtn.addEventListener('click', function () { 
-    timer = false; 
-    hour = 0; 
-    minute = 0; 
-    second = 0; 
-    count = 0; 
-    document.getElementById('hr').innerHTML = "00"; 
-    document.getElementById('min').innerHTML = "00"; 
-    document.getElementById('sec').innerHTML = "00"; 
-    document.getElementById('count').innerHTML = "00"; 
-}); 
-  
-function stopWatch() { 
-    if (timer) { 
-        count++; 
-  
-        if (count == 100) { 
-            second++; 
-            count = 0; 
-        } 
-  
-        if (second == 60) { 
-            minute++; 
-            second = 0; 
-        } 
-  
-        if (minute == 60) { 
-            hour++; 
-            minute = 0; 
-            second = 0; 
-        } 
-  
-        let hrString = hour; 
-        let minString = minute; 
-        let secString = second; 
-        let countString = count; 
-  
-        if (hour < 10) { 
-            hrString = "0" + hrString; 
-        } 
-  
-        if (minute < 10) { 
-            minString = "0" + minString; 
-        } 
-  
-        if (second < 10) { 
-            secString = "0" + secString; 
-        } 
-  
-        if (count < 10) { 
-            countString = "0" + countString; 
-        } 
-  
-        document.getElementById('hr').innerHTML = hrString; 
-        document.getElementById('min').innerHTML = minString; 
-        document.getElementById('sec').innerHTML = secString; 
-        document.getElementById('count').innerHTML = countString; 
-        setTimeout(stopWatch, 10); 
-    } 
-}
+const start = document.getElementById("start-btn");
+const stop = document.getElementById("stop-btn");
+const reset = document.getElementById("reset-btn");
+const lap = document.getElementById("lap-btn");
+const background=document.querySelector(".lap-content-container");
+const displayTime = document.querySelector(".output");
+const lapContainer = document.querySelector(".lap-container");
+const lapHeadings = document.querySelectorAll(".heading");
+let hr = document.getElementById("hr");
+let mint = document.getElementById("min");
+let sec = document.getElementById("sec");
+let mili = document.getElementById("count");
+start.style.display = "block";
+stop.style.display = "none";
+reset.style.display = "none";
+lap.style.display = "none";
 
-timeConvertor=(x)=>{
-    hr = x / (60*60*1000);
-    x = x - hr*(60*60*1000);
-    min = x / (60*1000);
-    x = x - min*(60*1000);
-    sec = x / 1000;
-    x = x - sec*1000;
-    console.log(hr+" ..."+min+" "+sec+" "+x);
-    return {hr,min,sec,x};
-}
+let startTime;
+let elapsedTime = 0;
+let timeInterval;
+let lapCounter = 0;
+let lapStartTime = 0;
+//handle stopwatch start function
+start.addEventListener("click", () => {
+    start.style.display = "none";
+    stop.style.display = "block";
+    reset.style.display = "block";
+    lap.style.display = "block";
+    startTime = Date.now() - elapsedTime;
+    timeInterval = setInterval(() => {
+        elapsedTime = Date.now() - startTime;
+        const time = timeFormate(elapsedTime);
+        const { hours, minutes, seconds, milliseconds } = time;
+        hr.innerHTML = digitHandler(hours);
+        mint.innerHTML = digitHandler(minutes);
+        sec.innerHTML = digitHandler(seconds);
+        mili.innerHTML = digitHandler(milliseconds);
+    }, 10);
+    lapStartTime = startTime;
+});
+//handle stopwatch pause function
+stop.addEventListener("click", () => {
+    clearInterval(timeInterval);
+    start.style.display = "block";
+    stop.style.display = "none";
+    reset.style.display = "block";
+    lap.style.display = "none";
+});
+//handle stopwatch reset function
+reset.addEventListener("click", () => {
+    clearInterval(timeInterval);
+    hr.innerHTML = "00";
+    mint.innerHTML = "00";
+    sec.innerHTML = "00";
+    mili.innerHTML = "00";
+    elapsedTime = 0;
+    totalLapElpasedTime = 0;
+    start.style.display = "block";
+    stop.style.display = "none";
+    reset.style.display = "none";
+    lap.style.display = "none";
+    lapCounter = 0;
+    lapHeadings[0].style.display = "none";
+    lapHeadings[1].style.display = "none";
+    lapHeadings[2].style.display = "none";
+    lapContainer.innerHTML = "";
+});
+//to handle stopwatch lap function
+lap.addEventListener("click", () => {
+    lapCounter++;
+    let { lapTime, totalLapTime } = getLapTime();
+    if (lapCounter > 0) {
+        background.style.display="flex";
+        lapHeadings[0].style.display = "block";
+        lapHeadings[1].style.display = "block";
+        lapHeadings[2].style.display = "block";
+    }
+    const lapDiv = document.createElement("div");
+    lapDiv.classList.add("lap");
+    const lapCounterDiv = document.createElement("div");
+    lapCounterDiv.classList.add("lap-number");
+    const lapTimeDiv = document.createElement("div");
+    lapTimeDiv.classList.add("lap-time");
+    const lapTotelTimeDiv = document.createElement("div");
+    lapTotelTimeDiv.classList.add("total-lap-time");
+    lapCounterDiv.innerHTML = `Lap ${digitHandler(lapCounter)}`;
+    displayFormattedTime(lapTimeDiv, lapTime);
+    displayFormattedTime(lapTotelTimeDiv, totalLapTime);
+    lapContainer.appendChild(lapDiv);
+    lapDiv.appendChild(lapCounterDiv);
+    lapDiv.appendChild(lapTimeDiv);
+    lapDiv.appendChild(lapTotelTimeDiv);
+});
+let totalLapElpasedTime = 0;
+//to get the lap time and totel lap time
+const getLapTime = () => {
+    const elapsedLapTime = Date.now() - lapStartTime;
+    const lapTime = timeFormate(elapsedLapTime);
+    lapStartTime = Date.now();
+    totalLapElpasedTime += elapsedLapTime;
+    const totalLapTime = timeFormate(totalLapElpasedTime);
+    return { lapTime, totalLapTime };
+};
+
+
+//convert the miliseconds into hours, mintues , seconds and miliseconds
+const timeFormate = (elapsedTime) => {
+    const date = new Date(elapsedTime);
+    let hours = date.getUTCHours();
+    let minutes = date.getUTCMonth();
+    let seconds = date.getUTCSeconds()
+    let milliseconds = Math.floor(date.getMilliseconds() / 10);
+    return { hours, minutes, seconds, milliseconds };
+};
+//to handle two digit number
+const digitHandler = (time) => {
+    if (time < 10) {
+        return `0${time}`;
+    } else {
+        return time;
+    }
+};
+//to display lap time formate
+const displayFormattedTime = (div, time) => {
+    const { hours, minutes, seconds, milliseconds } = time;
+    div.innerHTML = `${digitHandler(hours)}:${digitHandler(
+        minutes
+    )}:${digitHandler(seconds)}:${digitHandler(milliseconds)}`;
+};
